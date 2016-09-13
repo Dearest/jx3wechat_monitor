15b8472ea5955690bc291e38b23aebb49fd37d10
zhouajicheng <hanchen@126.com>
2016-09-13 19:18:17 +0800

    返回激活码

--- app/controllers/active_code_controller.rb
+++ app/controllers/active_code_controller.rb
@@ -1,6 +1,6 @@
 class ActiveCodeController < ApplicationController
   def code
-    codes = ActiveCode.select(:code).where(created_at: (Time.now.beginning_of_day..Time.now.end_of_day))
-    render text: code.map(&:code).join('|')
+    codes = ActiveCode.select(:code).where(created_at: (Time.now.beginning_of_day..Time.now.end_of_day)).map(&:code).join('|')
+    render plain: codes.present? ? codes : ''
   end
 end
